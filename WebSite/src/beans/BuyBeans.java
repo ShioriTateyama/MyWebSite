12cb8f89552d9ba7208a36c7c836901e5a100082
楯山詩織 <shiori@tatesanshiorinoMacBook-Pro.local>
2019-02-05 18:42:13 +0900

    sql

---
+++ WebSite/src/beans/BuyBeans.java
@@ -0,0 +1,42 @@
+package beans;
+
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class BuyBeans implements Serializable{
+	
+	private int buyId;
+	private int userId;
+	private int totalPrice;
+	private Date createDate;
+	
+	
+	public int getBuyId() {
+		return buyId;
+	}
+	public void setBuyId(int buyId) {
+		this.buyId = buyId;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public int getTotalPrice() {
+		return totalPrice;
+	}
+	public void setTotalPrice(int totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	
+	
+
+}
